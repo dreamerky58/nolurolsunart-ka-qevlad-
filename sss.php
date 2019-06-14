bd21218f52a01a3b7d56c0d3e51f9ea6d365aa64
dreamerky58 <48422013+dreamerky58@users.noreply.github.com>
2019-06-14 12:44:09 +0300

    Add files via upload

---
+++ sss.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE HTML>
+<html lang="tr-TR" dir="ltr">
+<head>
+</head>
+<body>
+<img src="http://www.minecraftpazari.com/img/ek/logo.png" alt="Örnek Resim">
+<h3><strong>Kredi Yükledikten Sonra Ne yapacağım ?</strong></h3>
+<h3>Kredi Yükledikten Sonra siteye tekrar giriş yapın.Sonrasında resimdeki örnek gibi <strong>Hemen Satın Al</strong> tuşuna basın</h3>
+<img src="http://i.hizliresim.com/mLpL8P.png" alt="Örnek Resim">
+<h3><strong>Hesap bilgilerini nereden Görebilirim ?</strong></h3>
+<img src="http://i.hizliresim.com/VYNY5y.png" alt="Örnek Resim">
+<h3><strong>Destek Bildirimi Nasıl Oluştururum?</strong></h3>
+<h3>Mail adresimizden(<a class="__cf_email__" href="cdn-cgi/l/email-protection" data-cfemail="b3dedaddd6d0c1d2d5c7c3d2c9d2c1daf3d4ded2dadf9dd0dcde">[email&#160;protected]</a><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script>) sipariş numaranızı belirterek yardım alabilirsiniz.Onun haricinde yardım oluşturmadan önce aşağıdaki yönergeleri takip edin.</h3>
+<h3>-Hesabınızın dns sorunu olabilir.Modemi kapatıp açın</h3>
+<h3>-Hesabınızda deneme sorunu yaşıyor olabilirsiniz.Modem kapat aç işlemini yapın</h3>
+<h3>-Mojang sitesi bazen çökebiliyor.Biraz bekleyip tekrar deneyin.</h3>
+</body>
+</html>
